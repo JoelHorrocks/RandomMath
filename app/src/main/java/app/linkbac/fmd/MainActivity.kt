3baedc0efddbb103cac7744f79128180770d07fc
JoelHorrocks <joel.horrocks@gmail.com>
2025-07-22 14:40:23 +0100

    Fixed inverted status bar

--- app/src/main/java/app/linkbac/fmd/MainActivity.kt
+++ app/src/main/java/app/linkbac/fmd/MainActivity.kt
@@ -3,6 +3,7 @@ package app.linkbac.fmd
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -39,6 +40,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        enableEdgeToEdge()
         setContent {
             val navController = rememberNavController()
 
